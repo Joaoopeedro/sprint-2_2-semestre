b9c422f150b3ca812adcf545483ee2fac57691d3
joao <joaopedro37@gmail.com>
2021-08-24 16:48:10 -0300

    feito a API

---
+++ Teorioco/Exemplos/senai_filme_webAPI/senai_filme_webAPI/Domains/FilmeDomains.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai_filme_webAPI.Domains
+{
+    public class FilmeDomains
+    {
+        public int idFilme { get; set; }
+        public int idGenero { get; set; }
+        public string tituloFilme { get; set; }
+
+    }
+}
