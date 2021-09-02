324435a7ef9928dc967b4e393b692e2b870297d7
joao <joaopedro37@gmail.com>
2021-09-01 22:11:25 -0300

    CRUD veiculo Finalizado

--- Exercicios/Exercicio.Rendal/scripts/SQLQuery1.sql
+++ Exercicios/Exercicio.Rendal/scripts/SQLQuery1.sql
@@ -12,3 +12,15 @@ INSERT INTO ALUGUEL(cod_cliente,cod_veic,dataRetirada,dataDev) VALUES (@cod_clie
 UPDATE ALUGUEL SET cod_cliente = 1, cod_veic = 2,dataRetirada = '03-09-2021 00:00:00.000',dataDev = '04-09-2021 00:00:00.000'	WHERE cod_aluguel = 1
 
 UPDATE ALUGUEL SET cod_cliente = 2, cod_veic = 3,dataRetirada = '04-09-2021 ',dataDev = '24-09-2021'	WHERE cod_aluguel = 1
+
+UPDATE VEICULO SET cod_empresa = 1, cod_mod = 1,placa = 'WWW'	WHERE cod_veic = 1
+UPDATE VEICULO SET cod_empresa = 2, cod_mod = 4,placa = 'JOA'	WHERE cod_veic = 1
+
+SELECT V.cod_veic, V.placa,M.nomemod, MA.nomeMar,E.nomeEmpresa FROM VEICULO V
+INNER JOIN EMPRESA E ON V.cod_empresa = E.cod_empresa
+INNER JOIN MODELO M ON V.cod_mod = M.cod_mod
+INNER JOIN MARCA MA ON M.cod_mar = MA.cod_mar
+WHERE cod_veic = 1
+
+
+INSERT INTO VEICULO(cod_empresa,cod_mod,placa) VALUES (@cod_empresa,@cod_mod,@placa)
